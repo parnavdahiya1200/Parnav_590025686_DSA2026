e9e635905baf86c1909aed2b517c43ad99cc5dd1
parnavdahiya1200 <parnavdahiya1200@gmail.com>
2026-03-25 21:30:55 +0530

    Update Q5.c

--- DSA_assingment_3/Q5.c
+++ DSA_assingment_3/Q5.c
@@ -1,7 +1,7 @@
 //5.Write a C program to convert a given infix expression (A+B*(C^D-E)^(F+G*H)-I) into its corresponding postfix expression (ABCD^E-FGH*+^*+I-) using a stack implemented with
 // arrays#include <stdio.h>
+#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 
 #define MAX 100
 
@@ -16,50 +16,37 @@ char pop() {
     return stack[top--];
 }
 
-int precedence(char x) {
-    if (x == '^') return 3;
-    if (x == '*' || x == '/') return 2;
+int priority(char x) {
+    if (x == '(') return 0;
     if (x == '+' || x == '-') return 1;
+    if (x == '*' || x == '/') return 2;
+    if (x == '^') return 3;
     return 0;
 }
 
 int main() {
-    char infix[MAX] = "A+B*(C^D-E)^(F+G*H)-I";
-    char postfix[MAX];
-    int i, j = 0;
+    char exp[] = "A+B*(C^D-E)^(F+G*H)-I";
+    char *e = exp;
+    char x;
 
-    for (i = 0; infix[i] != '\0'; i++) {
-
-        if (isalnum(infix[i])) {
-            postfix[j++] = infix[i];
+    while (*e != '\0') {
+        if (isalnum(*e)) {
+            printf("%c", *e);
+        } else if (*e == '(') {
+            push(*e);
+        } else if (*e == ')') {
+            while ((x = pop()) != '(')
+                printf("%c", x);
+        } else {
+            while (top != -1 && priority(stack[top]) >= priority(*e))
+                printf("%c", pop());
+            push(*e);
         }
-
-        else if (infix[i] == '(') {
-            push(infix[i]);
-        }
-
-        else if (infix[i] == ')') {
-            while (stack[top] != '(') {
-                postfix[j++] = pop();
-            }
-            pop();
-        }
-
-        else {
-            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
-                postfix[j++] = pop();
-            }
-            push(infix[i]);
-        }
+        e++;
     }
 
-    while (top != -1) {
-        postfix[j++] = pop();
-    }
-
-    postfix[j] = '\0';
-
-    printf("Postfix expression: %s\n", postfix);
+    while (top != -1)
+        printf("%c", pop());
 
     return 0;
 }
